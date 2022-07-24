4e58bbf9d0334fcdc3cb918ee1e5a8acf97e30fd
Alizberlin <81747094+Alizberlin@users.noreply.github.com>
2022-07-24 17:37:56 +0200

    lec 2 bis Tuple

--- Schwarzmuller/script.js
+++
@@ -1,9 +0,0 @@
-var button = document.getElementById('add-btn');
-var input1 = document.getElementById('button1');
-var input2 = document.getElementById('button2');
-var add = function (num1, num2) {
-    return num1 + num2;
-};
-button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
-    console.log(add(input1.value, input2.value));
-});
